b4999b516611d626a199adee5b8a644f247dade5
marooo326 <ktnqkr01@gmail.com>
2023-03-07 17:23:52 +0900

    feat: Nickname 변경 기능 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:tipme_front/screens/login_screen.dart';
+import 'package:tipme_front/widgets/home_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -23,91 +23,7 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       tabBuilder: (BuildContext context, int index) {
-        return CupertinoTabView(
-          builder: (BuildContext context) {
-            return Container(
-              child: index == 0
-                  ? Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const SizedBox(
-                          height: 100,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Text(
-                            "Welcome, ${apiData['nickname']}",
-                            style: const TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                        CupertinoListSection(
-                          header: const Text(
-                            'Menu',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          children: [
-                            CupertinoListTile.notched(
-                              title: const Text('닉네임 변경'),
-                              leading: const SizedBox(
-                                width: double.infinity,
-                                height: double.infinity,
-                                child: Icon(CupertinoIcons.arrow_2_circlepath),
-                              ),
-                              trailing: const CupertinoListTileChevron(),
-                              onTap: () => Navigator.of(context).push(
-                                CupertinoPageRoute(
-                                  builder: (BuildContext context) {
-                                    return const LoginScreen();
-                                  },
-                                ),
-                              ),
-                            ),
-                            const CupertinoListTile.notched(
-                              title: Text('받은 메세지'),
-                              leading: SizedBox(
-                                width: double.infinity,
-                                height: double.infinity,
-                                child:
-                                    Icon(CupertinoIcons.chat_bubble_text_fill),
-                              ),
-                              trailing: CupertinoListTileChevron(),
-                            ),
-                            const CupertinoListTile.notched(
-                              title: Text('친구 추가'),
-                              leading: SizedBox(
-                                width: double.infinity,
-                                height: double.infinity,
-                                child: Icon(CupertinoIcons
-                                    .person_crop_circle_badge_plus),
-                              ),
-                              trailing: CupertinoListTileChevron(),
-                            ),
-                            const CupertinoListTile.notched(
-                              title: Text('로그아웃'),
-                              leading: SizedBox(
-                                width: double.infinity,
-                                height: double.infinity,
-                                child: Icon(CupertinoIcons.xmark_circle),
-                              ),
-                              trailing: CupertinoListTileChevron(),
-                            ),
-                          ],
-                        )
-                      ],
-                    )
-                  : const Text(
-                      '0',
-                      style: TextStyle(fontSize: 20),
-                    ),
-            );
-          },
-        );
+        return HomeWidget(apiData: apiData);
       },
     );
   }
---
+++ lib/widgets/chagne_nickname_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangeNicknameWidget extends StatefulWidget {
+  const ChangeNicknameWidget({
+    super.key,
+  });
+
+  @override
+  State<ChangeNicknameWidget> createState() => _ChangeNicknameWidgetState();
+}
+
+class _ChangeNicknameWidgetState extends State<ChangeNicknameWidget> {
+  String guideMessage = "3~8자 사이의 닉네임을 입력해주세요.\n중복 및 공백은 허용되지 않습니다.";
+  bool isValid = true;
+  final _nicknameEditController = TextEditingController();
+  void changeGuide() {
+    setState(() {
+      isValid = false;
+      guideMessage = "다시 입력해주세요";
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nicknameEditController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoActionSheet(
+      title: Text(
+        guideMessage,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color:
+              isValid ? CupertinoColors.systemGreen : CupertinoColors.systemRed,
+        ),
+      ),
+      message: CupertinoTextField(
+        controller: _nicknameEditController,
+        textAlign: TextAlign.center,
+        maxLength: 8,
+        autofocus: true,
+      ),
+      actions: <CupertinoActionSheetAction>[
+        CupertinoActionSheetAction(
+          child: const Text('Change'),
+          onPressed: () {
+            if (_nicknameEditController.text.length < 3) {
+              //nick name 유효한지 검증하는 로직 추가 예정
+              changeGuide();
+            } else {
+              print(_nicknameEditController.text);
+              Navigator.pop(context);
+            }
+          },
+        ),
+        CupertinoActionSheetAction(
+          child: const Text(
+            'Cancle',
+            style: TextStyle(color: CupertinoColors.systemRed),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
