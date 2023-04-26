efffb6110a381049966557bec75bb18fcbe5bc1f
marooo326 <ktnqkr01@gmail.com>
2023-04-26 17:44:55 +0900

    feat: 환경설정 파일 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:tipme_front/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  final kakaoAppKey = dotenv.env['KAKAO_APP_KEY'];
+  final kakaoJsAppKey = dotenv.env['KAKAO_JS_APP_KEY'];
+
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
   WidgetsFlutterBinding.ensureInitialized();
-
   // runApp() 호출 전 Flutter SDK 초기화
   KakaoSdk.init(
-    nativeAppKey: 'f728522d88b590d29a643119029b65ef',
-    javaScriptAppKey: '10d524a455bcdb71f2c93c38a2d063e0',
+    nativeAppKey: kakaoAppKey,
+    javaScriptAppKey: kakaoJsAppKey,
   );
   runApp(const App());
 }
